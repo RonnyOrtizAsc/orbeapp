097593d1056729ab66f8d3b9180e130d222ab6ae
RonnyOrtizAsc <rtiz.ronny@gmail.com>
2026-09-16 15:36:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const SUPABASE_URL = "https://ijnetiyxrxxfhurlsnbc.supabase.co";
 const SUPABASE_KEY = "sb_publishable_dnjsWgsrPMUQov5JTJuthw_KEAqjMfK";
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-const CONTACTS_API_URL = "https://script.google.com/macros/s/AKfycby9Ri-XWAQBN5eibwiu3UnDDnrYRQNvty-Zyg0yV5ywsRG-HeyHweyGF1XMem_MlD9B/exec";
+const CONTACTS_API_URL = "https://script.google.com/macros/s/AKfycbySht8pQbWNIgZ76EOeDv9IP6fancWqTr4qMQ-H6CZ-wbqHMHmVBx2nYmPjjAmINXD6/exec";
 const CONTACTS_API_TOKEN = "cineronilio";
 
 // =====================================================
